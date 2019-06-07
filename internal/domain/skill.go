0908a15853ab55c62cda5c4aa792b42265a7ff56
Jim Wright <jim@linx.net>
2019-06-07 18:51:41 +0100

    Add formatted skill name to output

--- internal/domain/skill.go
+++ internal/domain/skill.go
@@ -1,18 +1,20 @@
 package domain
 
 type Skill struct {
-	Name       string
-	Rank       int
-	Level      int
-	Experience int
+	Name          string
+	FormattedName string
+	Rank          int
+	Level         int
+	Experience    int
 }
 
 func NewSkill(name string, rank int, level int, experience int) *Skill {
 	return &Skill{
-		Name:       name,
-		Rank:       rank,
-		Level:      level,
-		Experience: experience,
+		Name:          name,
+		FormattedName: FormatSkillName(name),
+		Rank:          rank,
+		Level:         level,
+		Experience:    experience,
 	}
 }
 
@@ -24,4 +26,58 @@ func OrderedSkills() []string {
 		"mining", "herblore", "agility", "theiving", "slayer",
 		"farming", "runecraft", "hunter", "construction",
 	}
+}
+
+func FormatSkillName(name string) string {
+	switch name {
+	case "overall":
+		return "Overall"
+	case "attack":
+		return "Attack"
+	case "defence":
+		return "Defence"
+	case "strength":
+		return "Strength"
+	case "hitpoints":
+		return "Hitpoints"
+	case "ranged":
+		return "Ranged"
+	case "prayer":
+		return "Prayer"
+	case "magic":
+		return "Magic"
+	case "cooking":
+		return "Cooking"
+	case "woodcutting":
+		return "Woodcutting"
+	case "fletching":
+		return "Fletching"
+	case "fishing":
+		return "Fishing"
+	case "firemaking":
+		return "Firemaking"
+	case "crafting":
+		return "Crafting"
+	case "smithing":
+		return "Smithing"
+	case "mining":
+		return "Mining"
+	case "herblore":
+		return "Herblore"
+	case "agility":
+		return "Agility"
+	case "theiving":
+		return "Theiving"
+	case "slayer":
+		return "Slayer"
+	case "farming":
+		return "Farming"
+	case "runecraft":
+		return "Runecraft"
+	case "hunter":
+		return "Hunter"
+	case "construction":
+		return "Construction"
+	}
+	return ""
 }
